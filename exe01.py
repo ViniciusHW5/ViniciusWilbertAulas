671d229fa256f2c4e5f8cb794d86d96b15cfedc9
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-04 21:19:41 -0300

    Update exe01.py

--- exe01.py
+++ exe01.py
@@ -2,3 +2,4 @@
 #idade: 17 anos
 #passa tempo: jogar jogos eletronicos tipo Cs1.6 ,CS ,CS 2 , Minecraft 
 #signo: peixe
+#Eu jogo no pc é jogo que mais jogo é o minecraft pq tem muita exploração é tem como modificar muito o jogo também
