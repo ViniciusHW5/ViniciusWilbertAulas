c074cd788cc9808bb850f1addf832921084d8dab
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-06 22:06:03 -0300

    codigo 1500

--- exe03.py
+++ exe03.py
@@ -1,1 +1,10 @@
 #exercício 03
+# Faça um programa que receba 1500 e tenha um amento no salario e tenha um amento de 5% do salario e a final exiba o salario com alemento
+salario = 1500
+aumento = 1500 / 100
+aumento = aumento * 5
+salario = salario + aumento 
+print (salario)
+#salario = 1500
+#aumento = 5
+#print(salario) + (salario + aumento /100)
